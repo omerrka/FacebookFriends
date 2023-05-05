f6757a8837105a8ed9c23bf4e8414a3d2d48814a
Omer Karabulut <omrkarabulut2@gmail.com>
2023-05-05 16:17:39 +0300

    initial commit

---
+++ FacebookFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FacebookFriends
+//
+//  Created by Aybike Zeynep Tiryaki on 5.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
